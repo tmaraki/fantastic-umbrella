83cfc4b29ba8fc39a4c056843b35b4fd36fb4369
tmaraki <toshiemaraki@gmail.com>
2024-03-02 10:16:33 -0500

    added comments to connections.js file regarding env variables

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,12 +1,15 @@
+//enables access to .env variables
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+//uses environment variables to connect to database
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
